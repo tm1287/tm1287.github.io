448f77ebc8752248390ecea82b8eb7ec62ba5f2c
Tejas Maraliga <tejas.maraliga@gmail.com>
2023-03-20 01:25:43 -0500

    Fix resume display

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,5 +1,9 @@
 import React, {useState} from 'react'
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import resume from "./Tejas_Maraliga_Resume.pdf"
+import { Document, Page, pdfjs} from "react-pdf/dist/esm/entry.webpack";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 function Resume() {
     const [numPages, setNumPages] = useState(null);
@@ -10,14 +14,14 @@ function Resume() {
     }
 
     return (
-        <div>
-        <Document file="./Tejas_Maraliga_Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
+        <Document
+            file={resume} 
+            onLoadError={(e) => console.error(e)} 
+            onLoadSuccess={onDocumentLoadSuccess}>
+            <div style={{marginTop: "20px", display: "flex", justifyContent: "center"}}>
+                <Page pageNumber={pageNumber} />
+            </div>
         </Document>
-        <p>
-            Page {pageNumber} of {numPages}
-        </p>
-        </div>
     );
 }
 
